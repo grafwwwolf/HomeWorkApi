0edc565b097c51e7d4dc2468475274b6f2874655
Stanislav <grafwwwolf@yandex.ru>
2021-10-07 17:43:17 +0300

    ДЗ улучшенное

--- src/main/java/Api.java
+++ src/main/java/Api.java
@@ -1,8 +1,17 @@
-
+import model.Cat;
+import model.Plate;
+import service.CatService;
 
 public class Api {
-    
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+        Plate plate = new Plate(100);
+        Cat catBarsik = new Cat("Барсик", 5);
+        Cat catMurzik = new Cat("Мурзик", 3);
+        Cat catBoris = new Cat("Борис", 7);
+        plate.info();
+        CatService catService = new CatService(catBarsik, catMurzik, catBoris);
+        catService.eatToSatiety(plate);
+        plate.info();
     }
 }
