0df439f09791c7923334eed910a817d0f72fc9dd
Stanislav <grafwwwolf@yandex.ru>
2022-01-27 18:35:06 +0300

    создание локальной истории - первая рабочая версия

--- ServerSide/src/main/java/ru/home/service/AuthenticationServiceImpl.java
+++ ServerSide/src/main/java/ru/home/service/AuthenticationServiceImpl.java
@@ -6,20 +6,15 @@ import ru.home.service.interfaces.AuthenticationService;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.List;
 
 public class AuthenticationServiceImpl implements AuthenticationService {
 
-    private List<UserEntity> userEntityList;
+
     private static Connection connection;
     private static Statement statement;
 
     public AuthenticationServiceImpl() {
-        this.userEntityList = new ArrayList<>();
-        userEntityList.add(new UserEntity("grafwwwolf", "password", "grafwwwolf"));
-        userEntityList.add(new UserEntity("robertrath", "password", "robertrath"));
-        userEntityList.add(new UserEntity("markel", "password", "markel"));
+
     }
 
     @Override
@@ -40,15 +35,6 @@ public class AuthenticationServiceImpl implements AuthenticationService {
         DBConnection.closeConnection();
     }
 
-//    @Override
-//    public String getNickNameByLoginAndPassword(String login, String password) {
-//        for (UserEntity userEntity: userEntityList) {
-//            if (userEntity.login.equals(login) && userEntity.password.equals(password)) {
-//                return userEntity.nickName;
-//            }
-//        }
-//        return null;
-//    }
 
     @Override
     public String getNickNameByLoginAndPassword(String login, String password) {
@@ -59,17 +45,4 @@ public class AuthenticationServiceImpl implements AuthenticationService {
             return null;
         }
     }
-
-    private class UserEntity {
-        private String login;
-        private String password;
-        private String nickName;
-
-        public UserEntity(String login, String password, String nickName) {
-            this.login = login;
-            this.password = password;
-            this.nickName = nickName;
-        }
-    }
 }
-//
